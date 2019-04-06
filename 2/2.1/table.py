d9d580fa51e5b3d598ea681be4d92bcfc93df28e
sergei kuksov <sergey.kuksov@func.ru>
2019-04-07 00:31:19 +0300

    upd

--- 2/2.1/table.py
+++ 2/2.1/table.py
@@ -4,12 +4,14 @@
 определяет его название.
 '''
 
+
 def table():
-    #  
+    #
 
     from random import randint
 
-    num = input('Введите атомный номер элемента или 0, чтобы завершить программу: ')
+    num = input(
+        'Введите атомный номер элемента или 0, чтобы завершить программу: ')
 
     if len(num) and num.isdigit():
         elem = ''
@@ -30,9 +32,10 @@ def table():
             r = randint(0, 3)
             table_data[r]
 
-            print(f'Такого номера нет. Введите другой номер, например {table_data[r]} или 0, чтобы завершить программу')
+            print(
+                f'Такого номера нет. Введите другой номер, например {table_data[r]} или 0, чтобы завершить программу')
             return table()
-    
+
         return print(f'Вы ввели номер {num}. Это атомный номер элемента {elem}')
 
     else:
@@ -45,4 +48,3 @@ def table():
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
-
